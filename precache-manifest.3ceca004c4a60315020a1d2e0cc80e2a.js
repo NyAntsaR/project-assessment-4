c417cda1d6911f1ce269c9e8e5dcdbf37606e6eb
Ny Antsa Rajaonarison <nyantsa@Nys-MacBook-Pro.local>
2019-10-07 15:50:01 -0700

    Updates

---
+++ precache-manifest.3ceca004c4a60315020a1d2e0cc80e2a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bab042dce86f418a42361d49339e001e",
+    "url": "/project-4-assessment/index.html"
+  },
+  {
+    "revision": "65f3513ce641e7b3ea5f",
+    "url": "/project-4-assessment/static/css/main.0e6c178c.chunk.css"
+  },
+  {
+    "revision": "4beab43a78b40ccaa2bd",
+    "url": "/project-4-assessment/static/js/2.b9d3aa2d.chunk.js"
+  },
+  {
+    "revision": "65f3513ce641e7b3ea5f",
+    "url": "/project-4-assessment/static/js/main.9cb2cf9d.chunk.js"
+  },
+  {
+    "revision": "75de8ec33a8a78802050",
+    "url": "/project-4-assessment/static/js/runtime-main.71652b97.js"
+  }
+]);
